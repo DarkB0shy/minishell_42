872230e7f208e298d5035f1252ee7f01a3ab69c4
Darkb0shy <dcarassi@localhost.localdomain>
2023-04-22 14:23:35 +0200

    ft_split

--- includes/minishell.h
+++ includes/minishell.h
@@ -11,6 +11,11 @@ typedef struct s_shell
 {
 	char	**env;
 	char	*pipeline;
+	char	**pipe_words;
 }	t_shell;
+
+// Libft
+int		ft_strlen(const char *str);
+char	**ft_split(const char *s, char c);
 
 # endif
--- srcs/main.c
+++ srcs/main.c
@@ -3,11 +3,16 @@
 int	main(int argc, char **argv, char **env)
 {
     t_shell shell;
+    int     i;
 
     (void)argc;
     (void)argv;
     (void)env;
+    i = -1;
     shell.pipeline = readline("minishell$");
-    printf("%s\n", shell.pipeline);
+    shell.pipe_words = ft_split(shell.pipeline, ' ');
+    while (shell.pipe_words[++i])
+        printf("%s\n", shell.pipe_words[i]);
+    free(shell.pipeline);
     return (0);
 }
